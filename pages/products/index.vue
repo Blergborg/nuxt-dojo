a7562fc5f2d3dcb3c0ed9755d25561e58f271996
Blergborg <pprater92@gmail.com>
2023-11-26 20:44:12 -0800

    Added 'products' subfolder of 'pages' folder. Added 'index.vue' to 'products' subfolder.

---
+++ pages/products/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <!-- the index.vue of a pages subfolder will be given a route of the subfolder's name -->
+        <!-- this will be found at "/products" route -->
+        <h1>Products</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+</style>
