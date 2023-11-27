e6df46c57028a969079d9ccc226e5f1ce207d4ac
Blergborg <pprater92@gmail.com>
2023-11-26 20:22:37 -0800

    Added a pages folder.
    Added about.vue and index.vue to pages folder.
    
    Removed app.vue since index.vue can now be our entrypoint.
    
    Nuxt takes care of routing automatically based on the pages folder.

--- app.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <!-- This is the default root component of the app. -->
-  <div>
-    <!-- NuxtWelcome is the default landing page for Nuxt, contained as a component that's easy to remove. -->
-    <!-- To see the app in action 'npm run dev' -->
-    <NuxtWelcome /> 
-    <p>Deez Nuxt</p>
-  </div>
-</template>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Home</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus eaque quibusdam quo alias praesentium doloremque ratione quaerat ipsam ut aperiam.</p>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+    h2 {
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+
+    p {
+        margin: 20px 0;
+    }
+</style>
