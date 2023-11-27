8af7c9d21184f70792ef3ac3614cf3a5e5be59e0
Blergborg <pprater92@gmail.com>
2023-11-26 19:00:55 -0800

    First commit

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true }
+  // This is where we can extend or change the default configuration.
+  // Can also store environment variables like api keys here.
+})
