e08b8bbee8c5a2c90bf69e6fb75a2a7639de33bf
Blergborg <pprater92@gmail.com>
2023-11-26 22:44:50 -0800

    Added 'layouts' folder.
    Added default.vue and products.vue to 'layouts'.
    
    Moved nav tag and NuxtLink contents to default.vue from root index.vue

---
+++ layouts/default.vue
@@ -0,0 +1,44 @@
+<!-- 
+    This is a bit special. Nuxt will automatically wrap pages with what's in this default.vue layout file.
+ -->
+<template>
+    <div>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <!-- Instead of anchor (<a>) tags, Nuxt gives us these link components. -->
+                        <!-- 
+                            A reason we do this is anchor tags make a new request to the server for link route.
+                            Nuxt is meant to behave as a SPA after it is loaded in the browser.
+                            NuxtLink components just swap out the components being displayed.
+                            NuxtLinks also come with some built-in styling classes that can be used to change how currently active/inactive links are displayed.
+                            -->
+                        <NuxtLink to="/">Home</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/about">About</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/products">Products</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </div>
+
+    <!-- Output the page content -->
+    <!-- Slot is a nuxt component that says "this is where the other stuff goes" -->
+    <div>
+        <slot />
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+    .router-link-exact-active {
+        color: #12b488;
+    }
+</style>
