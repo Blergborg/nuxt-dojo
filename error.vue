fd63332d605aba1856b2a13c2958a73ad45ebbfe
Blergborg <pprater92@gmail.com>
2023-11-28 10:19:00 -0800

    Added basic custom error page error.vue

---
+++ error.vue
@@ -0,0 +1,15 @@
+<!-- It's really just this simple. Nuxt looks for error.vue in the root of the project. -->
+<template>
+    <div class="mt-7 max-w-sm mx-auto text-center card">
+        <p class="mt-7 text-7xl font-bold">{{ error.statusCode }}</p>
+        <p class="mt-7 text-6xl ">Oops.</p>
+        <p class="mt-7">{{ error.message }}</p>
+    </div>
+</template>
+
+<script setup>
+    defineProps(['error'])
+</script>
+
+<style scoped>
+</style>
