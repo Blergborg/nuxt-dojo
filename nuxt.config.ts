1cadf36c2d8f35eae28b31bd0d0fc17ebfe491ca
Blergborg <pprater92@gmail.com>
2023-11-28 16:06:46 -0800

    Set up head content in nuxt.config.ts.
    Added "Add to Cart" button to show that Material Icons was linked.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,17 @@ export default defineNuxtConfig({
   // Can also store environment variables like api keys here.
   modules: [
     '@nuxtjs/tailwindcss'
-  ]
+  ],
+  // Can control head content here (meta tags, links to stylesheets, etc.)
+  app: {
+    head: {
+      title: 'Nuxt Dojo',
+      meta: [
+        {name: 'description', content: 'everything about Nuxt3'}
+      ],
+      link: [
+        {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Material+Icons'}
+      ]
+    }
+  }
 })
